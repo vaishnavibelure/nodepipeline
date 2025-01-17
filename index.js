64fa8590628dee467aa21055423b71dd5f053eb6
vaishnavibelure <belurevaishnavi@gmail.com>
2025-01-17 21:35:16 +0530

    from laptop

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('changed out ');
+ res.send('changed in ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
